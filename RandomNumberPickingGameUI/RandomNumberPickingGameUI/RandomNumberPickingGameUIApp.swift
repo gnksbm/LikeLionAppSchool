3a76a8dae69f7023ede72a22c74071ca9e197872
Geonseob Kim <gnksbm@gmail.com>
2023-06-20 02:33:23 +0900

    Add RandomNumberPickingGameUI

---
+++ RandomNumberPickingGameUI/RandomNumberPickingGameUI/RandomNumberPickingGameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberPickingGameUIApp.swift
+//  RandomNumberPickingGameUI
+//
+//  Created by gnksbm on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberPickingGameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
