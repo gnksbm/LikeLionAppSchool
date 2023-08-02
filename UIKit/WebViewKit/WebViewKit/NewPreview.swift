194e6f1b1d99d535748042cc43363b7bdc09a7ee
Geonseob Kim <gnksbm@gmail.com>
2023-08-02 11:18:39 +0900

    [feat] add WebViewKit

--- UIKit/WebViewKit/WebViewKit/NewPreview.swift
+++ UIKit/WebViewKit/WebViewKit/NewPreview.swift
@@ -8,46 +8,46 @@
 import UIKit
 import SwiftUI
 
-enum EnumTab: Int, CaseIterable {
-//MARK: - Set Here
-    case <#case#>, <#case#>, <#case#>
-
-    static var rootViewIndex = <#rootViewIndex#>
-
-    var info: VCInfo {
-        let title = "\(self)"
-        switch self {
-        case .home:
-            return VCInfo(title: title, systemImageName: "<#systemImageName#>", vc: <#ViewController#>())
-        case .chat:
-            return VCInfo(title: title, systemImageName: "<#systemImageName#>", vc: <#ViewController#>())
-        case .others:
-            return VCInfo(title: title, systemImageName: "<#systemImageName#>", vc: <#ViewController#>())
-        @unknown case _:
-            print("This is not defined case: \(self)")
-            break
-        }
-    }
-//MARK: - Logic
-
-    struct VCInfo {
-        var title: String
-        var systemImageName: String
-        var vc: UIViewController
-    }
-    
-    static func makeAllCasesToViewControllers() -> [UINavigationController] {
-        var viewControllers: [UINavigationController] = []
-        
-        Self.allCases.forEach {
-            let viewController = UINavigationController(rootViewController: $0.info.vc)
-            viewController.tabBarItem = UITabBarItem(title: $0.info.title, image: UIImage(systemName: $0.info.systemImageName), tag: $0.rawValue)
-            viewControllers.append(viewController)
-        }
-        
-        return viewControllers
-    }
-}
+//enum EnumTab: Int, CaseIterable {
+////MARK: - Set Here
+//    case <#case#>, <#case#>, <#case#>
+//
+//    static var rootViewIndex = <#rootViewIndex#>
+//
+//    var info: VCInfo {
+//        let title = "\(self)"
+//        switch self {
+//        case .home:
+//            return VCInfo(title: title, systemImageName: "<#systemImageName#>", vc: <#ViewController#>())
+//        case .chat:
+//            return VCInfo(title: title, systemImageName: "<#systemImageName#>", vc: <#ViewController#>())
+//        case .others:
+//            return VCInfo(title: title, systemImageName: "<#systemImageName#>", vc: <#ViewController#>())
+//        @unknown case _:
+//            print("This is not defined case: \(self)")
+//            break
+//        }
+//    }
+////MARK: - Logic
+//
+//    struct VCInfo {
+//        var title: String
+//        var systemImageName: String
+//        var vc: UIViewController
+//    }
+//
+//    static func makeAllCasesToViewControllers() -> [UINavigationController] {
+//        var viewControllers: [UINavigationController] = []
+//
+//        Self.allCases.forEach {
+//            let viewController = UINavigationController(rootViewController: $0.info.vc)
+//            viewController.tabBarItem = UITabBarItem(title: $0.info.title, image: UIImage(systemName: $0.info.systemImageName), tag: $0.rawValue)
+//            viewControllers.append(viewController)
+//        }
+//
+//        return viewControllers
+//    }
+//}
 
 struct UIKitVCPreview: UIViewControllerRepresentable {
     
@@ -85,21 +85,21 @@ struct UIKitNaviVCPreview: UIViewControllerRepresentable {
 
 //MARK: - MainTabbar, MainTabbar_Preview
 
-class MainTabbar: UITabBarController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+//class MainTabbar: UITabBarController {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        self.viewControllers = EnumTab.makeAllCasesToViewControllers()
+//        self.selectedIndex = EnumTab.rootViewIndex
+//        self.tabBar.tintColor = .black
+//        self.tabBar.backgroundColor = .white
+//    }
+//}
 
-        self.viewControllers = EnumTab.makeAllCasesToViewControllers()
-        self.selectedIndex = EnumTab.rootViewIndex
-        self.tabBar.tintColor = .black
-        self.tabBar.backgroundColor = .white
-    }
-}
-
-struct MainTabbar_Preview: PreviewProvider {
-    static var previews: some View {
-        UIKitVCPreview {
-            MainTabbar()
-        }
-    }
-}
+//struct MainTabbar_Preview: PreviewProvider {
+//    static var previews: some View {
+//        UIKitVCPreview {
+////            MainTabbar()
+//        }
+//    }
+//}
--- UIKit/WebViewKit/WebViewKit/ViewController.swift
+++ UIKit/WebViewKit/WebViewKit/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
     }
 
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.x.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
 }
 
+extension ViewController: WKUIDelegate {
+    
+}
+
+import SwiftUI
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        UIKitVCPreview {
+            ViewController()
+        }
+    }
+}
